c64962bcbec3d440e5441bd2784e8682a374761f
RonaldCBC <ronald@cbccreative.com>
2022-11-14 21:27:17 -0600

    initial work on the homepage navigation template

---
+++ js/homepage-navigation.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+  el_autohide = document.querySelector('.autohide');
+
+  if (el_autohide) {
+    var last_scroll_top = 0;
+    el_autohide.classList.add('initial-scrolled');
+    window.addEventListener('scroll', function () {
+      let scroll_top = window.scrollY;
+      if (scroll_top == 0) {
+        el_autohide.classList.add('initial-scrolled');
+        el_autohide.classList.remove('scrolled-up');
+      } else if (scroll_top < last_scroll_top) {
+        el_autohide.classList.remove('scrolled-down');
+        el_autohide.classList.add('scrolled-up');
+      } else {
+        el_autohide.classList.remove('scrolled-up');
+        el_autohide.classList.add('scrolled-down');
+      }
+      last_scroll_top = scroll_top;
+    });
+    // window.addEventListener
+  }
+  // if
+});
+// DOMContentLoaded  end
