b53b5103bab0a91e92906029b44bb492f541179e
RonaldCBC <ronald@cbccreative.com>
2022-11-19 15:26:37 -0600

    paused navigation development to focus on footer and homepage completion

--- js/core.js
+++ js/core.js
@@ -1,16 +1,18 @@
-// Custom Navication JS
-document.addEventListener('DOMContentLoaded', function () {
-  el_autohide = document.querySelector('.autohide');
+// Setting up custom media queries
+const mediaQuery1200 = window.matchMedia('(min-width: 1200px)');
+const mediaQuery992 = window.matchMedia('(min-width: 992px)');
 
-  if (el_autohide) {
+// Custom Global Navication JS
+const el_autohide = document.querySelector('#main-navbar');
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Check if the media query 1200px is true
+  if (mediaQuery1200.matches) {
+    // Run Code
     var last_scroll_top = 0;
-    el_autohide.classList.add('initial-scrolled');
     window.addEventListener('scroll', function () {
       let scroll_top = window.scrollY;
-      if (scroll_top == 0) {
-        el_autohide.classList.add('initial-scrolled');
-        el_autohide.classList.remove('scrolled-up');
-      } else if (scroll_top < last_scroll_top) {
+      if (scroll_top < last_scroll_top) {
         el_autohide.classList.remove('scrolled-down');
         el_autohide.classList.add('scrolled-up');
       } else {
@@ -21,7 +23,28 @@ document.addEventListener('DOMContentLoaded', function () {
     });
     // window.addEventListener
   }
-  // if
 }); // DOMContentLoaded  end
 
-// Custom Navication JS end
+// Custom Global Navication JS end
+
+// Custom Footer JS
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Check if the media query 992px is true
+  if (mediaQuery992.matches) {
+    let headerIndex;
+    let menuIndex;
+    let footerToggle = document.getElementsByClassName('footer-menu-heading');
+    let footerMenu = document.getElementsByClassName('footer-list-menu');
+
+    for (headerIndex = 0; headerIndex < footerToggle.length; headerIndex++) {
+      footerToggle[headerIndex].setAttribute('aria-expanded', true);
+    }
+
+    for (menuIndex = 0; menuIndex < footerMenu.length; menuIndex++) {
+      footerMenu[menuIndex].classList.add('show');
+    }
+  }
+}); // DOMContentLoaded  end
+
+// Custom Footer JS end
