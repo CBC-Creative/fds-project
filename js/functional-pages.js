7108787e1e65466b1902afaa5de51a30220110bd
RonaldCBC <ronald@cbccreative.com>
2023-01-17 20:39:51 -0600

    Reviewed and Approved my-cart, my-cart-checkout, my-cart-checkout-options, my-cart-order-confirmation, and my-account templates

--- js/functional-pages.js
+++ js/functional-pages.js
@@ -1,20 +1,23 @@
 const mediaQuerymax767 = window.matchMedia('(max-width: 767.98px)');
-      document.addEventListener('DOMContentLoaded', function () {
-      var htmltofind = document.getElementsByClassName('myaccount-active');
-      var sethtml = document.getElementsByClassName('active-menu-mobile');      
+
+document.addEventListener('DOMContentLoaded', function () {
+  var htmltofind = document.getElementsByClassName('myaccount-active');
+  var sethtml = document.getElementsByClassName('active-menu-mobile');
+
   for (let htm = 0; htm < htmltofind.length; htm++) {
     var gethtml = htmltofind[htm].innerHTML;
     for (let stm = 0; stm < sethtml.length; stm++) {
       sethtml[stm].innerHTML = gethtml;
-    }   
+    }
   }
+
   if (mediaQuerymax767.matches) {
     var mobilesidenavclick = document.getElementsByClassName('mobile-navigationmyaccount');
     var addactiveclass = document.getElementsByClassName('myaccount-nav');
     for (let nv = 0; nv < mobilesidenavclick.length; nv++) {
       mobilesidenavclick[nv].addEventListener('click', function () {
-        addactiveclass[0].classList.toggle('active');     
-    });
-  }
+        addactiveclass[0].classList.toggle('active');
+      });
+    }
   }
 });
