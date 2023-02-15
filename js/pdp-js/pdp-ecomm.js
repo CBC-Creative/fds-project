3360a38441c722805b5c6b2c0d6ea4b8ac040d78
CBCCreativeDev <dev@cbccreative.com>
2023-02-15 07:20:03 -0500

    feat: pdp ecomm template

--- js/pdp-js/pdp-ecomm.js
+++ js/pdp-js/pdp-ecomm.js
@@ -1,6 +1,24 @@
-    
-    $('.product-image-list img').click(function(){
-        var selectedPath = $(this).attr("src");
 
-        $('.product-banner-image  img').attr('src', selectedPath);
- });
+$('.product-image-list img').click(function () {
+    var selectedPath = $(this).attr("src");
+
+    $('.product-banner-image  img').attr('src', selectedPath);
+});
+$('.product-size-btn').click(function () {
+    //alert(this)
+    $('.product-size-btn').removeClass('active');
+    if ($(this).hasClass('active')) {
+        $(this).removeClass('active');
+    } else {
+        $(this).addClass('active');
+    }
+});
+$('.colors-selected .btn-floating').click(function () {
+    //alert(this)
+    $('.colors-selected .btn-floating').removeClass('selected-color');
+    if ($(this).hasClass('selected-color')) {
+        $(this).removeClass('selected-color');
+    } else {
+        $(this).addClass('selected-color');
+    }
+});
