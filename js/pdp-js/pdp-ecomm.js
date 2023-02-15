10e7cb07b4f07ac7640b1a29ac9d8e1b48e5c60c
RonaldCBC <ronald@cbccreative.com>
2023-02-15 16:15:10 -0600

    PDP Ecomm Updates for 2/15/2023 Finished

--- js/pdp-js/pdp-ecomm.js
+++ js/pdp-js/pdp-ecomm.js
@@ -1,24 +1,33 @@
+// Setup Media Queries
+const mq768 = window.matchMedia('(min-width: 768px)');
+
+$(document).ready(function () {
+  if (mq768.matches) {
+    const ecommGallery = document.querySelector('.ecommerce-gallery .multi-carousel');
+    $(ecommGallery).addClass('vertical');
+  }
+});
 
 $('.product-image-list img').click(function () {
-    var selectedPath = $(this).attr("src");
+  var selectedPath = $(this).attr('src');
 
-    $('.product-banner-image  img').attr('src', selectedPath);
+  $('.product-banner-image  img').attr('src', selectedPath);
 });
 $('.product-size-btn').click(function () {
-    //alert(this)
-    $('.product-size-btn').removeClass('active');
-    if ($(this).hasClass('active')) {
-        $(this).removeClass('active');
-    } else {
-        $(this).addClass('active');
-    }
+  //alert(this)
+  $('.product-size-btn').removeClass('active');
+  if ($(this).hasClass('active')) {
+    $(this).removeClass('active');
+  } else {
+    $(this).addClass('active');
+  }
 });
 $('.colors-selected .btn-floating').click(function () {
-    //alert(this)
-    $('.colors-selected .btn-floating').removeClass('selected-color');
-    if ($(this).hasClass('selected-color')) {
-        $(this).removeClass('selected-color');
-    } else {
-        $(this).addClass('selected-color');
-    }
+  //alert(this)
+  $('.colors-selected .btn-floating').removeClass('selected-color');
+  if ($(this).hasClass('selected-color')) {
+    $(this).removeClass('selected-color');
+  } else {
+    $(this).addClass('selected-color');
+  }
 });
