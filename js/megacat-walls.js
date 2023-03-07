17aa521ab166041ddcdeb6d3208113daa37ac8ce
CBCCreativeDev <dev@cbccreative.com>
2023-03-07 07:15:12 -0500

    Add ing landing-trendwall js changes.

--- js/megacat-walls.js
+++ js/megacat-walls.js
@@ -1,86 +1,20 @@
-$('.swatch-right-boxes-main .swatch-right-boxes').click(function () {
-  var getmainchild = $(this).children().last();
-var getswatchtitlename = getmainchild.attr('data-name');
-var getcategoryname = $(this).children().last().attr('data-category');
-$('.swatch-left-preview-box .swatch-glazing-title span[data-name]').attr('data-name', getswatchtitlename)
-$('.swatch-left-preview-box .swatch-glazing-title span[data-name]').text(getswatchtitlename)
-$('.swatch-left-preview-box .swatch-title span[data-category]').attr('data-category', getcategoryname)
-$('.swatch-left-preview-box .swatch-title span[data-category]').text(getcategoryname)
-
-var swatchimgtopreview = $(this).children().first().children().attr('src');
-$('.swatch-preview-img img').attr('src', swatchimgtopreview)
+var swatchimgtopreview = ''; 
+var x = '';
+var y = '';
+$(document).on('click','.swatch-right-boxes-main-row .swatch-right-boxes', function (e) {
+    e.preventDefault();
+    x = $(this).parent().find(".swatch-small-title-right").attr("data-category");
+    y = $(this).parent().find(".swatch-small-title-right").attr("data-name");
+    $(this).parents(".accordion-body").find('.swatch-left-preview-box .swatch-title span[data-name]').attr('data-name', y)
+    $(this).parents(".accordion-body").find('.swatch-left-preview-box .swatch-title span[data-name]').text(y)
+    $(this).parents(".accordion-body").find('.swatch-left-preview-box .swatch-title span[data-category]').attr('data-category', x)
+    $(this).parents(".accordion-body").find('.swatch-left-preview-box .swatch-title span[data-category]').text(x)
+    swatchimgtopreview = $(this).parent().find('.swatch-img img').attr('src');
+    $(this).parents(".accordion-body").find('.swatch-preview-img img').attr('src', swatchimgtopreview);
 });
+
 $( document ).ready(function() {
   $('.swatch-right-boxes-main-row .col-lg-2:first-child .swatch-right-boxes').trigger( "click" );
-});
-
-
-$('.swatch-right-boxes-main .swatch-right-boxes-paint').click(function () {
-  var getmainchild1 = $(this).children().last();
-  var getswatchtitlename1 = getmainchild1.attr('data-paint-name');
-  var getcategoryname1 = $(this).children().last().attr('data-category');
-
-  $('.swatch-left-preview-box-paint-trim .swatch-paint-title span[data-paint-name]').attr('data-paint-name', getswatchtitlename1)
-  $('.swatch-left-preview-box-paint-trim .swatch-paint-title span[data-paint-name]').text(getswatchtitlename1)
-  $('.swatch-left-preview-box-paint-trim .swatch-title span[data-category]').attr('data-category', getcategoryname1)
-  $('.swatch-left-preview-box-paint-trim .swatch-title span[data-category]').text(getcategoryname1)
-
-  var swatchimgtopreview1 = $(this).children().first().children().attr('src');
-  $('.swatch-preview-img-paint img').attr('src', swatchimgtopreview1)
-});
-$( document ).ready(function() {
-  $('.swatch-right-boxes-main-row .col-lg-2:first-child .swatch-right-boxes-paint').trigger( "click" );
-});
-$('.swatch-right-boxes-main .swatch-right-boxes-panel-fabric').click(function () {
-  var getmainchild2 = $(this).children().last();
-  var getswatchtitlename2 = getmainchild2.attr('data-panel-name');
-  var getcategoryname2 = $(this).children().last().attr('data-category');
-  $('.swatch-left-preview-box .swatch-panel-fabric-title span').attr('data-panel-name', getswatchtitlename2)
-  $('.swatch-left-preview-box .swatch-panel-fabric-title span').text(getswatchtitlename2)
-  $('.swatch-left-preview-box-panel-fabric .swatch-title span[data-category]').attr('data-category', getcategoryname2)
-  $('.swatch-left-preview-box-panel-fabric .swatch-title span[data-category]').text(getcategoryname2)
-
-  var swatchimgtopreview2 = $(this).children().first().children().attr('src');
-  $('.swatch-preview-img-panel-fabric img').attr('src', swatchimgtopreview2)
-});
-
-$( document ).ready(function() {
-  $('.swatch-right-boxes-main-row .col-lg-2:first-child .swatch-right-boxes-panel-fabric').trigger( "click" );
-});
-
-$('.swatch-right-boxes-main .swatch-right-boxes-panel-door').click(function () {
-  var getmainchild3 = $(this).children().last();
-  var getswatchtitlename3 = getmainchild3.attr('data-panel-door-name');
-  var getcategoryname3 = $(this).children().last().attr('data-category');
-  $('.swatch-left-preview-box .swatch-panel-door-title span[data-panel-door-name]').attr('data-panel-door-name', getswatchtitlename3)
-  $('.swatch-left-preview-box .swatch-panel-door-title span[data-panel-door-name]').text(getswatchtitlename3)
-  $('.swatch-left-preview-box-panel-door-vinyl .swatch-title span[data-category]').attr('data-category', getcategoryname3)
-  $('.swatch-left-preview-box-panel-door-vinyl .swatch-title span[data-category]').text(getcategoryname3)
-
-  var swatchimgtopreview3 = $(this).children().first().children().attr('src');
-  $('.swatch-preview-img-panel-door img').attr('src', swatchimgtopreview3)
-});
-
-$( document ).ready(function() {
-  $('.swatch-right-boxes-main-row .col-lg-2:first-child .swatch-right-boxes-panel-door').trigger( "click" );
-});
-
-
-$('.swatch-right-boxes-main .swatch-right-boxes-laminate').click(function () {
-  var getmainchild4 = $(this).children().last();
-  var getswatchtitlename4 = getmainchild4.attr('data-laminate-name');
-  var getcategoryname4 = $(this).children().last().attr('data-category');
-  $('.swatch-left-preview-box .swatch-laminate-title span').attr('data-laminate-name', getswatchtitlename4)
-  $('.swatch-left-preview-box .swatch-laminate-title span').text(getswatchtitlename4)
-  $('.swatch-left-preview-box-laminate .swatch-title span[data-category]').attr('data-category', getcategoryname4)
-  $('.swatch-left-preview-box-laminate .swatch-title span[data-category]').text(getcategoryname4)
-
-  var swatchimgtopreview4 = $(this).children().first().children().attr('src');
-  $('.swatch-preview-img-laminate img').attr('src', swatchimgtopreview4)
-});
-
-$( document ).ready(function() {
-  $('.swatch-right-boxes-main-row .col-lg-2:first-child .swatch-right-boxes-laminate').trigger( "click" );
 });
 
 $(document).ready(function(){
@@ -180,13 +114,17 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 });
 function equalHeight() {
-  var wallcaption = Array.from(document.getElementsByClassName('wall-slider-caption'));
-
-  for (let w = 0; w < wallcaption.length; w++) {
-    var setheightcaption = Math.max(wallcaption[0].scrollHeight);
-    wallcaption[w].style.height = setheightcaption + 'px';
+  if ($("body").find("#primary-content").hasClass("wall-slider-caption")) {
+    var wallcaption = Array.from(document.getElementsByClassName('wall-slider-caption'));
+    for (let w = 0; w < wallcaption.length; w++) {
+      var setheightcaption = Math.max(wallcaption[0].scrollHeight);
+      wallcaption[w].style.height = setheightcaption + 'px';
+    }
   }
-  var wallindicators = document.getElementsByClassName('wallsliderindicators');
-  wallindicators[0].style.bottom = setheightcaption - 58 + 'px';
+  
+  if ($("body").find("#primary-content").hasClass("wallsliderindicators")) {
+    var wallindicators = document.getElementsByClassName('wallsliderindicators');
+    wallindicators[0].style.bottom = setheightcaption - 58 + 'px';
+  }  
 }
 equalHeight();
