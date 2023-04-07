11ef1d67297d0dbf84d567dc4c93e70f541232b1
Vitali <63733022+digitalbutlers@users.noreply.github.com>
2023-04-07 07:51:59 +0300

    feat: refactoring Awards & Recognition

--- js/db-awards.js
+++ js/db-awards.js
@@ -1,1 +1,11 @@
-import"./db-modulepreload-polyfill.js";/* empty css    */import{D as r,T as d}from"./db-bootstrap.esm.js";const o=document.getElementById("awards-tabs"),s=o.querySelectorAll(".nav-link"),c=document.querySelector('.dropdown-menu[data-name="awards"]'),e=document.querySelector('.dropdown-toggle[data-bs-toggle="dropdown"]'),i=()=>{new r(e),c.addEventListener("click",function(t){const n=t.target.dataset.value;e.textContent=t.target.textContent,document.querySelector(".dropdown-item.active").classList.remove("active"),t.target.classList.add("active"),Array.from(s).find(a=>a.href.includes(n)).click()})};i();const l=()=>{new d(o)};l();
+const awardsDropDownMenu = document.querySelector('.awards-dropdown-menu');
+const navLinks = awardsDropDownMenu.querySelectorAll('.dropdown-item[data-mdb-toggle="tab"]');
+const dropdownButton = awardsDropDownMenu.querySelector('.dropdown-toggle[data-mdb-toggle="dropdown"');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    navLinks.forEach(link => link.addEventListener('click', function (event) {
+        dropdownButton.textContent = this.textContent;
+        dropdownButton.click();
+    }))
+})
