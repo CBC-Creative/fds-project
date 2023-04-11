f2ae1713ec7eae2714b4ca06899ff2b5c69e42df
RonaldCBC <ronald@cbccreative.com>
2023-04-11 12:47:51 -0500

    added fixes to navigation and country selector page

--- js/functional-country-selector.js
+++ js/functional-country-selector.js
@@ -2,8 +2,13 @@ $('.choose-country-dropdown-select').click(function () {
   $('.country-list-div').toggleClass('d-block');
 });
 $('.country-list-div ul li').click(function () {
-    var selectedcountry = $(this).html();
-    console.log(selectedcountry);
-    $('.choose-country-dropdown-select').html(selectedcountry)
-    $('.country-list-div').removeClass('d-block');
-})
+  var selectedcountry = $(this).html();
+  console.log(selectedcountry);
+  $('.choose-country-dropdown-select').html(selectedcountry);
+  $('.country-list-div').removeClass('d-block');
+});
+
+var date = new Date();
+var year = date.getFullYear();
+
+document.getElementById('copyright-year').innerHTML = year;
