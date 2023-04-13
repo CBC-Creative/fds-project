cdfec6cfdbc5da77902bc807bcf41dbda455c234
Vitali <63733022+digitalbutlers@users.noreply.github.com>
2023-04-13 11:38:44 +0300

    feat: Change tabs to scrollspy

--- js/db-overview.js
+++ js/db-overview.js
@@ -1,33 +1,33 @@
-/** ***** Tabs ***** **/
-const initTabs = () => {
-  const tabs = document.getElementById('overview-tabs');
-  const navLinks = tabs.querySelectorAll('.nav-link');
+// /** ***** Tabs ***** **/
+// const initTabs = () => {
+//   const tabs = document.getElementById('overview-tabs');
+//   const navLinks = tabs.querySelectorAll('.nav-link');
 
-  function disableTabs() {
-    navLinks.forEach((link) => {
-      link.removeAttribute('data-mdb-toggle');
-    });
-  }
+//   function disableTabs() {
+//     navLinks.forEach((link) => {
+//       link.removeAttribute('data-mdb-toggle');
+//     });
+//   }
 
-  function enableTabs() {
-    navLinks.forEach((link) => {
-      link.setAttribute('data-mdb-toggle', 'tab');
-    });
-  }
+//   function enableTabs() {
+//     navLinks.forEach((link) => {
+//       link.setAttribute('data-mdb-toggle', 'tab');
+//     });
+//   }
 
-  window.addEventListener('load', () => {
-    if (window.innerWidth < 480) {
-      disableTabs();
-    }
-  });
+//   window.addEventListener('load', () => {
+//     if (window.innerWidth < 480) {
+//       disableTabs();
+//     }
+//   });
 
-  window.addEventListener('resize', () => {
-    if (window.innerWidth < 480) {
-      disableTabs();
-    } else {
-      enableTabs();
-    }
-  });
-};
+//   window.addEventListener('resize', () => {
+//     if (window.innerWidth < 480) {
+//       disableTabs();
+//     } else {
+//       enableTabs();
+//     }
+//   });
+// };
 
-initTabs();
+// initTabs();
