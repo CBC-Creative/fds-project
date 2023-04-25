d3cb5f27556e3312333a23179988d493772a1b06
RonaldCBC <ronald@cbccreative.com>
2023-04-25 17:34:19 -0500

    Udpated Homepage per Fellowes Request

--- js/rising.js
+++ js/rising.js
@@ -82,28 +82,3 @@ $(document).ready(function () {
     });
   });
 });
-
-window.addEventListener('load', videoScroll);
-window.addEventListener('scroll', videoScroll);
-
-function videoScroll() {
-  if (document.querySelectorAll('video[autoplay]').length > 0) {
-    var windowHeight = window.innerHeight,
-      videoEl = document.querySelectorAll('video[autoplay]');
-
-    for (var i = 0; i < videoEl.length; i++) {
-      var thisVideoEl = videoEl[i],
-        videoHeight = thisVideoEl.clientHeight,
-        videoClientRect = thisVideoEl.getBoundingClientRect().top;
-
-      if (
-        videoClientRect <= windowHeight - videoHeight * 0.5 &&
-        videoClientRect >= 0 - videoHeight * 0.5
-      ) {
-        thisVideoEl.play();
-      } else {
-        thisVideoEl.pause();
-      }
-    }
-  }
-}
