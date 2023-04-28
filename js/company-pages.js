e22ab2fc30d700d2f00509499deeddfdf9659cad
CBCCreativeDev <dev@cbccreative.com>
2023-04-28 07:48:14 -0400

    adding scrollanimation on company heritage page

--- js/company-pages.js
+++ js/company-pages.js
@@ -12,21 +12,34 @@ $(window).scroll(function() { if ($(window).scrollTop() > origOffsetY) {
 $('body').css('marginTop', '0px');
 }});
 
+
+// $('.heritage-nav-section a').click(function () {
+// $('.heritage-nav-section a').removeClass('active');
+// if ($(this).hasClass('active')) {
+//   $(this).removeClass('active');
+// } else{
+//   $(this).addClass('active');
+// }
+// })
+
 function checkElementLocation() {
   var $window = $(window);
-  var bottom_of_window = $window.scrollTop() + $window.height();
+  var bottom_of_window = $window.scrollTop();
 
   $('.section-content').each(function(i) {
     var $that = $(this);
-    var bottom_of_object = $that.position().top + ($that.outerHeight() / 5);
+    var bottom_of_object = $that.position().top + ($that.outerHeight() / 6);
     var bottom_of_object1 = $that.position().top + ($that.outerHeight() * 1.4);
     
+      // if (bottom_of_window > bottom_of_object2) {
+      //   var findid = $(this).attr('id');
+      //   $('.heritage-nav a').removeClass('active');
+      //   $('.heritage-nav a[href="#'+findid+'"]').addClass('active');
+      // }
     //if element is in viewport, add the animate class
     if (bottom_of_window > bottom_of_object && bottom_of_window < bottom_of_object1) {
       $(this).addClass('fadeIn');
-      // var findid = $(this).attr('id');
-      // $('.heritage-nav a').removeClass('active');
-      // $('.heritage-nav a[href="#'+findid+'"]').addClass('active');
+     
     } else{
       $(this).removeClass('fadeIn');
       
@@ -40,15 +53,5 @@ $(window).on('scroll', function() {
   checkElementLocation();
  
 });
-
-$('.heritage-nav-section a').click(function () {
-$('.heritage-nav-section a').removeClass('active');
-if ($(this).hasClass('active')) {
-  $(this).removeClass('active');
-} else{
-  $(this).addClass('active');
-}
-})
-
 
 
