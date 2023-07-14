a20b4dd3ef29290b5fc495dd5d33160a05b13327
RonaldCBC <ronald@cbccreative.com>
2023-07-14 11:31:21 -0500

    Updates to History and Details page

---
+++ js/fpp-details.js
@@ -0,0 +1,6 @@
+function expand() {
+  $('.collapse').collapse('show');
+}
+function collapse() {
+  $('.collapse').collapse('hide');
+}
--- js/fpp-order.js
+++
@@ -1,4 +0,0 @@
-// $(document).on('click', '#reset-filters-btn', function () {
-//   // Reset the form
-//   document.getElementById('filter-form').reset();
-// });
