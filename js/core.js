6a9de9ecf5114f4f6f17ec86201d10670e3dca5e
RonaldCBC <ronald@cbccreative.com>
2023-11-01 09:26:55 -0500

    Cart Functionality added to FDS Navigation

--- js/core.js
+++ js/core.js
@@ -263,3 +263,44 @@ $(window).on('load', function () {
     }
   });
 });
+
+$(document).ready(function () {
+  const cartTrigger = document.querySelector('#fpp-cart-trigger');
+  const cartTriggerLink = document.querySelector('#fpp-cart-trigger a');
+  const cartDropdown = document.querySelector('.cart-dropdown-div');
+  const cartClose = document.querySelector('.cart-header-close-btn');
+
+  let openCart = function () {
+    $(cartTrigger).addClass('openned');
+    $(cartDropdown).addClass('show');
+    $(cartTriggerLink).attr('aria-expanded', 'true');
+  };
+
+  let closeCart = function () {
+    $(cartTrigger).removeClass('openned');
+    $(cartDropdown).removeClass('show');
+    $(cartTriggerLink).attr('aria-expanded', 'false');
+  };
+
+  $(cartTrigger).click(function (event) {
+    openCart();
+    event.stopPropagation();
+  });
+
+  $(cartClose).click(function (event) {
+    closeCart();
+    event.stopPropagation();
+  });
+
+  $(window).scroll(function () {
+    if ($(cartDropdown).hasClass('show')) {
+      closeCart();
+    }
+  });
+
+  $('body').click(function (event) {
+    if (event.target !== cartDropdown && !cartDropdown.contains(event.target)) {
+      closeCart();
+    }
+  });
+});
