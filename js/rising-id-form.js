7bc5a403ccd2e48846225061c03804cce9e7a57d
RonaldCBC <ronald@cbccreative.com>
2023-11-08 16:45:00 -0600

    Rising Form Template Fully Ready for Review

--- js/rising-id-form.js
+++ js/rising-id-form.js
@@ -1,10 +1,24 @@
 $(document).ready(function () {
   let stepper = new mdb.Stepper(document.getElementById('rising-stepper-form'));
 
-  const lastStep = document.getElementById('step-6');
+  const stepFive = document.getElementById('step-5');
+  const stepSix = document.getElementById('step-6');
   const prevBtn = document.getElementById('form-example-prev-step');
   const nextBtn = document.getElementById('form-example-next-step');
   const resetBtn = document.getElementById('form-restart');
+
+  const allProducts = document.querySelectorAll('.product-selection-wrapper input[type="radio"]');
+
+  $(allProducts).each(function () {
+    $(this).on('click', function () {
+      let dataCustomLogo = $(this).data('custom-logo');
+      if ($(this).is(':checked') && dataCustomLogo == false) {
+        $(stepFive).addClass('disabled-step');
+      } else {
+        $(stepFive).removeClass('disabled-step');
+      }
+    });
+  });
 
   $(resetBtn).on('click', function () {
     $('#primary-content').attr('data-form-completed', 'false');
@@ -12,7 +26,7 @@ $(document).ready(function () {
   });
 
   let classListner = setInterval(function () {
-    if ($(lastStep).hasClass('stepper-active')) {
+    if ($(stepSix).hasClass('stepper-active')) {
       $(nextBtn).css('opacity', '0');
     } else {
       $(nextBtn).css('opacity', '1');
