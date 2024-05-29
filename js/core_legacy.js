5641be8ecf8b53ce7fa8af50aac0c3e4ac76c094
RonaldCBC <ronald@cbccreative.com>
2024-05-29 16:26:47 -0500

    Updated File Naming Structure for New JS/CSS files

--- js/core_legacy.js
+++ js/core_legacy.js
@@ -16,6 +16,111 @@ $(window).on('load', function () {
   var opennavbar = document.getElementsByClassName('navbar');
 
   // Custom Global Navigation JS Begin
+  // If at least 1200px or wider, remove MDBootstrap dropdown toggle attribute
+  if (mediaQuery1200.matches) {
+    var dropdown = document.querySelectorAll('.navbar .dropdown-toggle');
+    for (let v = 0; v < dropdown.length; v++) {
+      dropdown[v].removeAttribute('data-mdb-toggle');
+    }
+  }
+  // If 1199.98px or less, do this:
+  if (mediaQuerymax1200.matches) {
+    var navContainers = document.querySelectorAll('.navbar .dropdown-toggle');
+    var removenavactive = document.querySelectorAll('.navbar .mobile-back-btn');
+    var navlink = document.querySelectorAll('.navbar-nav .nav-item');
+    var closenav = document.querySelectorAll('.navbar .navbar-toggler');
+    var subnav = document.querySelectorAll('.navbar .sub-dropdown');
+    var subnavmenu = document.querySelectorAll('.navbar .sub-dropdown-menu');
+    var dropdownitem = document.querySelectorAll('.navbar .dropdown-item');
+    var navbarCollapse = document.querySelector('#navbarSupportedContent');
+
+    // Hides an open mobile nav on scroll
+    $(window).scroll(function () {
+      if ($(navbarCollapse).hasClass('show')) {
+        $(navbarCollapse).addClass('mobile-navbar-collapse-onscroll');
+        setTimeout(function () {
+          $(navbarCollapse).removeClass('show');
+          $(navbarCollapse).removeClass('mobile-navbar-collapse-onscroll');
+        }, 300);
+      }
+    });
+
+    // Loop through all Tier 1 Nav items
+    for (var i = 0; i < navContainers.length; i++) {
+      // Get each Dropdown Instance, dispose of the old instance, and Reinitialize it
+      $(navContainers[i]).dropdown();
+
+      // Whenever you click on a Tier 1 Nav Item on Mobile, Open its Subnav menu
+      navContainers[i].addEventListener('click', function () {
+        for (var ij = 0; ij < navlink.length; ij++) {
+          navlink[ij].classList.add('active');
+        }
+      });
+    }
+
+    // Whenever you click on the "Back" link in each Subnav, close the subnav menu
+    for (var n = 0; n < removenavactive.length; n++) {
+      removenavactive[n].addEventListener('click', function () {
+        for (var ik = 0; ik < navlink.length; ik++) {
+          navlink[ik].classList.remove('active');
+        }
+      });
+    }
+
+    // This closes any open subnav if you close the mobile menu with the hamburger menu
+    for (var k = 0; k < closenav.length; k++) {
+      closenav[k].addEventListener('click', function () {
+        for (var im = 0; im < navlink.length; im++) {
+          navlink[im].classList.remove('active');
+        }
+      });
+    }
+
+    // When on mobile, this stop the clicking of a submenu item from also activiating the parent link when clicked.
+    for (var a = 0; a < dropdownitem.length; a++) {
+      dropdownitem[a].addEventListener('click', function (e) {
+        e.stopPropagation();
+      });
+    }
+
+    // When clicking on a tier 2 nav item that has a subnav, this prevents the link from activating and shows the submenu instead
+    for (let l = 0; l < subnav.length; l++) {
+      subnav[l].addEventListener('click', function (e) {
+        e.preventDefault();
+        subnavmenu[l].classList.toggle('showsub');
+        subnav[l].classList.toggle('open');
+      });
+    }
+  }
+
+  // Show/Hide Nav on Scroll JS
+  // Check if the media query 1200px is true
+  if (mediaQuery1200.matches && el_autohide) {
+    var scroll = $(document).scrollTop();
+    var navHeight = $(el_autohide).outerHeight();
+
+    $(window).on('scroll', function () {
+      var scrolled = $(document).scrollTop();
+
+      if (scrolled > scroll) {
+        $(el_autohide).addClass('scrolled-down');
+        $(el_autohide).removeClass('scrolled-up');
+      } else {
+        $(el_autohide).addClass('scrolled-up');
+        $(el_autohide).removeClass('scrolled-down');
+      }
+
+      if (scrolled > navHeight) {
+        $(el_autohide).addClass('scrolled-down');
+        $(el_autohide).removeClass('top-scrolled-up');
+      } else {
+        $(el_autohide).addClass('top-scrolled-up');
+        $(el_autohide).removeClass('scrolled-down scrolled-up');
+      }
+
+      scroll = $(document).scrollTop();
+    });
+  }
 
   // Show/Hide Search Dropdown JS
   for (let s = 0; s < searchtab.length; s++) {
@@ -123,7 +228,10 @@ $(window).on('load', function () {
           videoHeight = thisVideoEl.clientHeight,
           videoClientRect = thisVideoEl.getBoundingClientRect().top;
 
-        if (videoClientRect <= windowHeight - videoHeight * 0.5 && videoClientRect >= 0 - videoHeight * 0.5) {
+        if (
+          videoClientRect <= windowHeight - videoHeight * 0.5 &&
+          videoClientRect >= 0 - videoHeight * 0.5
+        ) {
           thisVideoEl.play();
         } else {
           thisVideoEl.pause();
