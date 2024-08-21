55f2c74419d0af20557d40f9caa9651a48ed384e
RonaldCBC <ronald@cbccreative.com>
2024-08-21 13:26:11 -0500

    Added custom Play Button and Image to EDI Decomm page

--- js/edi.js
+++ js/edi.js
@@ -1,4 +1,15 @@
 $(window).on('load', function () {
   const myModal = new mdb.Modal(document.getElementById('ediVideo'), {});
   myModal.toggle();
+
+  let videoPlay = document.querySelector('.video-play-wrapper');
+  if (videoPlay) {
+    let ediVid = document.querySelector('#edi-video');
+    $(videoPlay).click(function () {
+      $(ediVid).trigger('play');
+    });
+    $(ediVid).on('play', function () {
+      $(videoPlay).addClass('hide');
+    });
+  }
 });
