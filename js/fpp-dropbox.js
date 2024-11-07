841e1dc296e15af49aa34bfd8f3ac24cb161b78a
RonaldCBC <ronald@cbccreative.com>
2024-11-07 09:38:29 -0600

    Added select all, row selected, and icon hover states

--- js/fpp-dropbox.js
+++ js/fpp-dropbox.js
@@ -3,6 +3,35 @@ const mediaQuerymin1200 = window.matchMedia('(min-width: 1200px)');
 
 document.addEventListener('DOMContentLoaded', function () {
   const dbuiLinks = document.querySelectorAll('.dbui-list-group-item');
+  const dbuiCheckAll = document.querySelector('#select-all-checks');
+  const dbuiCheckboxes = document.querySelectorAll('.dbui-row-check-input');
+
+  function toggleCheckboxes(dbuiCheckAll) {
+    dbuiCheckboxes.forEach((checkbox) => {
+      checkbox.checked = dbuiCheckAll;
+      let row = checkbox.parentNode.parentNode.parentNode;
+      if (checkbox.checked) {
+        row.classList.add('selected');
+      } else {
+        row.classList.remove('selected');
+      }
+    });
+  }
+
+  dbuiCheckAll.addEventListener('change', function () {
+    toggleCheckboxes(this.checked);
+  });
+
+  dbuiCheckboxes.forEach((checkbox) => {
+    checkbox.addEventListener('click', function () {
+      let row = checkbox.parentNode.parentNode.parentNode;
+      if (checkbox.checked) {
+        row.classList.add('selected');
+      } else {
+        row.classList.remove('selected');
+      }
+    });
+  });
 
   dbuiLinks.forEach((element) => {
     element.addEventListener('click', function () {
