1875526a5d5c05a9ab9fd7d9e0090a83d428d16a
RonaldCBC <ronald@cbccreative.com>
2024-12-03 08:46:43 -0600

    PP Dropbox UI Navigation Update ready for review

--- js/fpp-dropbox.js
+++ js/fpp-dropbox.js
@@ -41,33 +41,4 @@ document.addEventListener('DOMContentLoaded', function () {
       element.classList.add('active');
     });
   });
-
-  if (mediaQuerymax1200.matches) {
-    let dbuiDropdown = document.querySelectorAll('.dbui-dropdown');
-    let dbuiDropdownInner = document.querySelectorAll('.dbui-dropdown-inner');
-    const dbuiListGroup = document.querySelectorAll('.dbui-list-group');
-
-    dbuiDropdown.forEach((element) => {
-      element.addEventListener('click', function () {
-        dbuiListGroup.forEach((el) => {
-          el.classList.toggle('show');
-        });
-      });
-    });
-
-    dbuiLinks.forEach((element) => {
-      element.addEventListener('click', function () {
-        let ddInnerHTML = element.innerHTML;
-        dbuiDropdownInner.forEach((el) => {
-          el.innerHTML = ddInnerHTML;
-        });
-        dbuiListGroup.forEach((el) => {
-          el.classList.remove('show');
-        });
-      });
-    });
-  }
-
-  if (mediaQuerymin1200.matches) {
-  }
 });
